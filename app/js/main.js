47dedd185b1b90d8c3b300109d667ee38a87abd7
phivu99 <lephivu99@gmail.com>
2023-07-20 08:29:49 +0700

    up

--- app/js/main.js
+++ app/js/main.js
@@ -490,3 +490,14 @@
 
 })(jQuery);
 
+$(document).ready(function() {
+    var str = $('.curved-text').html();
+    var curved = '';
+    for (var i = 0, len = str.length; i < len; i++) {
+        curved += '<div><span class="char'; curved += i; curved += '">';
+        curved += str[i];
+        curved += '</span>';
+        curved += '</div>';
+    }
+    $('.curved-text').html(curved);
+});
